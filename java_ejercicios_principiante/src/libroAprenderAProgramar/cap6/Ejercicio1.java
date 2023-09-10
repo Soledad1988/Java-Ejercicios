b33a484242b78643113ca21e558bbf0367fb5f33
Brenda <brentorres26@gmail.com>
2023-09-10 17:02:11 -0300

    Reorganizacion de codigo

---
+++ java_ejercicios_principiante/src/libroAprenderAProgramar/cap6/Ejercicio1.java
@@ -0,0 +1,13 @@
+package libroAprenderAProgramar.cap6;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		char c = '\u2661';
+		char b = 9825;
+		
+		System.out.println(c);
+		System.out.println(b);
+	}
+}
