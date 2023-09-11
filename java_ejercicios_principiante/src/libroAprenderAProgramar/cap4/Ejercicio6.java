5cc2bb438502c64cca9b5032cfb630c692932a7a
Brenda <brentorres26@gmail.com>
2023-09-11 19:12:22 -0300

    Ejercicio capitulo 4

--- java_ejercicios_principiante/src/libroAprenderAProgramar/cap4/Ejercicio6.java
+++ java_ejercicios_principiante/src/libroAprenderAProgramar/cap4/Ejercicio6.java
@@ -3,4 +3,32 @@ package libroAprenderAProgramar.cap4;
 //Diseñar una funcion que nos diga si un número es primo
 public class Ejercicio6 {
 
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<=15;i++) {
+			if(esPrimo(i)) {
+				System.out.println(i + " Es primo");
+			}else{
+				System.out.println(i + " Es compuesto");
+			}
+		}
+	}
+	
+	public static boolean esPrimo(int num) {
+		boolean primo = true;
+		int i = 2;
+		
+		if(num <2 ) {
+			primo = false;
+		}
+		
+		while(i < num  && primo == true) {
+			if(num%i==0) {
+				primo = false;
+			}
+			i++;
+		}
+		
+		return primo;
+	}
 }
