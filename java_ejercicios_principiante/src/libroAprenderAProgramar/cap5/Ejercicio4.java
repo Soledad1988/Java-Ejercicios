49e8190cbd3323ff0fd4666b95736ea27ca107e1
Brenda <brentorres26@gmail.com>
2023-09-12 10:44:05 -0300

    Ejercicios capitulo 5

--- java_ejercicios_principiante/src/libroAprenderAProgramar/cap5/Ejercicio4.java
+++ java_ejercicios_principiante/src/libroAprenderAProgramar/cap5/Ejercicio4.java
@@ -1,5 +1,46 @@
 package libroAprenderAProgramar.cap5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Implmentar un programa que inicialice una tabla con nuestros numeros favoritos.
+//S continuacion pedir al usuario el indice de un elemento que sera eliminado de la tabla
+//Continuaremos eliminndo elementos hasta que el indice introducido sea negativo
+//o hasta que no existan mas elementos que borrar. Es imprecindible controlar
+//que el indice leido corresponde a un dato  valido
 public class Ejercicio4 {
 
+	//Genera un bucle infinito
+	public static void main(String[] args) {
+		Scanner p = new Scanner(System.in);
+		int tabla[] = {10, 8, -2, 96};
+		
+		int numElemento = tabla.length;
+		
+		//mostramos loe elemntos de la tabla:
+		System.out.println(Arrays.toString(tabla));
+		
+		System.out.println("Indice del elemnto a borrar: ");
+		int indiceBorrar = p.nextInt();
+		
+		while(indiceBorrar >=0 && numElemento != 0) {
+			if(indiceBorrar < numElemento) {
+				tabla[indiceBorrar] = tabla[numElemento - 1];
+				numElemento--;
+				mostrarTabla(tabla, numElemento);
+			}else {
+				System.out.println("No existen elemntos para borrar");
+			}
+			System.out.println("Indice del elemento a borrar: ");
+			indiceBorrar = p.hashCode();
+		}
+	}
+	
+	static void mostrarTabla(int a[], int n) {
+		System.out.print("[");
+		for(int i = 0; i < n; i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.print("]");
+	}
 }
--- java_ejercicios_principiante/src/libroAprenderAProgramar/cap5/Ejercicio5.java
+++ java_ejercicios_principiante/src/libroAprenderAProgramar/cap5/Ejercicio5.java
@@ -1,5 +1,68 @@
 package libroAprenderAProgramar.cap5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Desarrolla el juego la camara secerta, que consiste abrir una camara mediante su combinacion secreta
+//que esta formada por una combinacion de digitos del 1 al 5.
+//El jugador espedificara cual es la longitud de la combinacion, a mayor longitud mayor dificultad en el juego
+//La aplicacion genera de forma aleatoria una combimacion secreta que el usuario tendra que acertar.
+//En cada  intento se muestra como pista, para cada digito  de la combinacion introdicida  por el jugador
+//si es mayor, menor o igual que el correspondiente en la combinacion secreta
 public class Ejercicio5 {
 
+	public static void main(String[] args) {
+		Scanner p = new Scanner(System.in);
+		
+		System.out.print("Ingresar la longitud: ");
+		int longitud = p.nextInt();
+		
+		int combinacionSecreta[] = new int [longitud];
+		int combinacionJugador [] = new int[longitud];
+		
+		generarCombinacion(combinacionSecreta);
+		System.out.println(Arrays.toString(combinacionSecreta));//muestra la combinacion generada aleatoriamente
+		System.out.println("Escriba una combinacion: ");
+		leerTabla(combinacionJugador);
+		
+		while(!Arrays.equals(combinacionSecreta, combinacionJugador)) {
+			System.out.println("Escriba una combinacion: ");
+			leerTabla(combinacionJugador);
+		}
+		
+		System.out.println("La camara esta abierta!!!");
+	}
+	
+	
+	
+	//función generar combinacion
+	static void generarCombinacion(int t[]) {
+		final int MAXIMO = 5;
+			for(int i = 0; i < t.length; i++) {
+				t[i] = (int)(Math.random()*MAXIMO+1);
+			}
+	}
+	
+	//función recorrer tabla
+	static void leerTabla(int t[]) {
+		Scanner p = new Scanner(System.in);
+		for(int i = 0; i<t.length;i++) {
+			t[i] = p.nextInt();
+		}
+	}
+	
+	//funcion mostrar pistas:
+	static void mostrarPistas(int secerto[], int jugador[]) {
+		for(int i = 0; i<jugador.length;i++) {
+			System.out.print(jugador[i]);
+			
+			if(secerto[i] > jugador[i]) {
+				System.out.println("Mayor");
+			}else if(secerto[i] < jugador[i]) {
+				System.out.println("Menor");
+			}else {
+				System.out.println("Iguales");
+			}
+		}
+	}
 }
