8183e663e7e1faa234f2806f6e8d84e146c8881d
Brenda <brentorres26@gmail.com>
2024-01-02 16:04:29 -0300

    Ejercicio Capitulo 1

---
+++ java_ejercicios_principiante/src/libro200Ejercicios/cap1/Ejercicio1.java
@@ -0,0 +1,10 @@
+package libro200Ejercicios.cap1;
+
+//Escribe un programa que muestre tu nombre por pantalla
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sol");
+	}
+}
