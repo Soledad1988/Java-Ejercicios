9634cad7d4337c2a63d135fe213a8e14225e148f
Brenda <brentorres26@gmail.com>
2024-03-18 17:49:22 -0300

    Ejercicios capitulo 1 al 8

---
+++ java_ejercicios_principiante/src/libro200Ejercicios/cap8/Ejercicio1.java
@@ -0,0 +1,20 @@
+package libro200Ejercicios.cap8;
+
+//Crear un abiblioteca de funciones matemáticas que contenga las siguientes funciones:
+// 1 esCapicua
+// 2 esPrimo
+// 3 siguientePrimo
+// 4 potencia
+// 5 digitos
+// 6 voltea
+// 7 digitoN
+// 8 posicionDeDigito
+// 9 quitaPorDetras
+// 10 quitaPorDelante
+// 11 pegaPorDetras
+// 12 pegaPorDelante
+// 13 trozoDeNumero
+// 14 juntaNumeros
+public class Ejercicio1 {
+
+}
